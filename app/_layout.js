856155a79b546ef5f84c3b9efbdf4f47cc783728
Muhamad Dani Setiawan <muhamaddanisetiawan@gmail.com>
2024-04-21 12:55:30 +0700

    refactor: minor changes to change home screen

--- app/_layout.js
+++ app/_layout.js
@@ -37,156 +37,156 @@ const Layout = () => {
     return null;
   }
   return (
-    // <Tabs
-    //   screenOptions={{
-    //     tabBarStyle: {
-    //       position: "absolute",
-    //       bottom: 0,
-    //       right: 0,
-    //       left: 0,
-    //       elevation: 0,
-    //       height: 60,
-    //       background: "#fff",
-    //     },
-    //   }}
-    //   tabBarStyle={{
-    //     position: "absolute",
-    //     bottom: 0,
-    //     right: 0,
-    //     left: 0,
-    //     elevation: 0,
-    //     height: 60,
-    //     background: "#fff",
-    //   }}
-    //   onLayout={onLayoutRootView}
-    // >
-    //   <Tabs.Screen
-    //     name="index"
-    //     options={{
-    //       title: "Home",
-    //       tabBarLabelStyle: {
-    //         fontFamily: "PlusJakartaSansMedium",
-    //         fontSize: 12,
-    //         textTransform: "none",
-    //       },
-    //       tabBarActiveTintColor: "#F37548",
-    //       tabBarIcon: ({ focused }) => {
-    //         return (
-    //           <View style={{ alignItems: "center", justifyContent: "center" }}>
-    //             <Foundation
-    //               name="home"
-    //               size={24}
-    //               color={focused ? "#F37548" : "#B3B9C4"}
-    //             />
-    //           </View>
-    //         );
-    //       },
-    //     }}
-    //   />
-    //   <Tabs.Screen
-    //     name="riwayat"
-    //     options={{
-    //       title: "Riwayat",
-    //       tabBarLabelStyle: {
-    //         fontFamily: "PlusJakartaSansMedium",
-    //         fontSize: 12,
-    //         textTransform: "none",
-    //       },
-    //       tabBarActiveTintColor: "#F37548",
-    //       tabBarIcon: ({ focused }) => {
-    //         return (
-    //           <View style={{ alignItems: "center", justifyContent: "center" }}>
-    //             <MaterialIcons
-    //               name="library-books"
-    //               size={24}
-    //               color={focused ? "#F37548" : "#B3B9C4"}
-    //             />
-    //           </View>
-    //         );
-    //       },
-    //     }}
-    //   />
-    //   <Tabs.Screen
-    //     name="qris"
-    //     options={{
-    //       title: "",
-    //       tabBarLabelStyle: {
-    //         fontFamily: "PlusJakartaSansMedium",
-    //         fontSize: 12,
-    //         textTransform: "none",
-    //         marginBottom: 8,
-    //       },
-    //       tabBarActiveTintColor: "#F37548",
-    //       tabBarIcon: ({ focused }) => {
-    //         return (
-    //           <View
-    //             style={{
-    //               alignItems: "center",
-    //               justifyContent: "center",
-    //               width: Platform.OS === "ios" ? 50 : 60,
-    //               height: Platform.OS === "ios" ? 50 : 60,
-    //               borderRadius: Platform.OS === "ios" ? 25 : 30,
-    //               backgroundColor: focused ? "#F37548" : "#fff",
-    //               top: Platform.OS === "ios" ? 5 : -20,
-    //             }}
-    //           >
-    //             <Image
-    //               source={icons.icQris}
-    //               style={{ width: 70, height: 70 }}
-    //             />
-    //           </View>
-    //         );
-    //       },
-    //     }}
-    //   />
-    //   <Tabs.Screen
-    //     name="favorite"
-    //     options={{
-    //       title: "Favorite",
-    //       tabBarLabelStyle: {
-    //         fontFamily: "PlusJakartaSansMedium",
-    //         fontSize: 12,
-    //         textTransform: "none",
-    //       },
-    //       tabBarActiveTintColor: "#F37548",
-    //       tabBarIcon: ({ focused }) => {
-    //         return (
-    //           <View style={{ alignItems: "center", justifyContent: "center" }}>
-    //             <Octicons
-    //               name="star-fill"
-    //               size={24}
-    //               color={focused ? "#F37548" : "#B3B9C4"}
-    //             />
-    //           </View>
-    //         );
-    //       },
-    //     }}
-    //   />
-    //   <Tabs.Screen
-    //     name="pengaturan"
-    //     options={{
-    //       title: "Pengaturan",
-    //       tabBarLabelStyle: {
-    //         fontFamily: "PlusJakartaSansMedium",
-    //         fontSize: 12,
-    //         textTransform: "none",
-    //       },
-    //       tabBarActiveTintColor: "#F37548",
-    //       tabBarIcon: ({ focused }) => {
-    //         return (
-    //           <View style={{ alignItems: "center", justifyContent: "center" }}>
-    //             <FontAwesome6
-    //               name="gear"
-    //               size={22}
-    //               color={focused ? "#F37548" : "#B3B9C4"}
-    //             />
-    //           </View>
-    //         );
-    //       },
-    //     }}
-    //   />
-    // </Tabs>
-   <AlertTest/>
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          position: "absolute",
+          bottom: 0,
+          right: 0,
+          left: 0,
+          elevation: 0,
+          height: 60,
+          background: "#fff",
+        },
+      }}
+      tabBarStyle={{
+        position: "absolute",
+        bottom: 0,
+        right: 0,
+        left: 0,
+        elevation: 0,
+        height: 60,
+        background: "#fff",
+      }}
+      onLayout={onLayoutRootView}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarLabelStyle: {
+            fontFamily: "PlusJakartaSansMedium",
+            fontSize: 12,
+            textTransform: "none",
+          },
+          tabBarActiveTintColor: "#F37548",
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View style={{ alignItems: "center", justifyContent: "center" }}>
+                <Foundation
+                  name="home"
+                  size={24}
+                  color={focused ? "#F37548" : "#B3B9C4"}
+                />
+              </View>
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="riwayat"
+        options={{
+          title: "Riwayat",
+          tabBarLabelStyle: {
+            fontFamily: "PlusJakartaSansMedium",
+            fontSize: 12,
+            textTransform: "none",
+          },
+          tabBarActiveTintColor: "#F37548",
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View style={{ alignItems: "center", justifyContent: "center" }}>
+                <MaterialIcons
+                  name="library-books"
+                  size={24}
+                  color={focused ? "#F37548" : "#B3B9C4"}
+                />
+              </View>
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="qris"
+        options={{
+          title: "",
+          tabBarLabelStyle: {
+            fontFamily: "PlusJakartaSansMedium",
+            fontSize: 12,
+            textTransform: "none",
+            marginBottom: 8,
+          },
+          tabBarActiveTintColor: "#F37548",
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: Platform.OS === "ios" ? 50 : 60,
+                  height: Platform.OS === "ios" ? 50 : 60,
+                  borderRadius: Platform.OS === "ios" ? 25 : 30,
+                  backgroundColor: focused ? "#F37548" : "#fff",
+                  top: Platform.OS === "ios" ? 5 : -20,
+                }}
+              >
+                <Image
+                  source={icons.icQris}
+                  style={{ width: 70, height: 70 }}
+                />
+              </View>
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="favorite"
+        options={{
+          title: "Favorite",
+          tabBarLabelStyle: {
+            fontFamily: "PlusJakartaSansMedium",
+            fontSize: 12,
+            textTransform: "none",
+          },
+          tabBarActiveTintColor: "#F37548",
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View style={{ alignItems: "center", justifyContent: "center" }}>
+                <Octicons
+                  name="star-fill"
+                  size={24}
+                  color={focused ? "#F37548" : "#B3B9C4"}
+                />
+              </View>
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="pengaturan"
+        options={{
+          title: "Pengaturan",
+          tabBarLabelStyle: {
+            fontFamily: "PlusJakartaSansMedium",
+            fontSize: 12,
+            textTransform: "none",
+          },
+          tabBarActiveTintColor: "#F37548",
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View style={{ alignItems: "center", justifyContent: "center" }}>
+                <FontAwesome6
+                  name="gear"
+                  size={22}
+                  color={focused ? "#F37548" : "#B3B9C4"}
+                />
+              </View>
+            );
+          },
+        }}
+      />
+    </Tabs>
+   
   );
 };
 
