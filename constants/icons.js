0f8ecc5b4c3f4f1765173e8d55331992b3d9cf51
adeboratiansi <adeboratiansi@gmail.com>
2024-04-23 19:41:01 +0700

    feat: add Status Berhasil Terkirim Screen UI

--- constants/icons.js
+++ constants/icons.js
@@ -41,6 +41,7 @@ import icArrowRightPoin from "../assets/icons/ic-arrow-right-poin.png";
 import icPaste from "../assets/icons/ic-paste-sertakan-laporan.png";
 import icPaste2 from "../assets/icons/ic-paste2-sertakan-laporan.png";
 import icX from "../assets/icons/ic-x-sertakan-laporan.png";
+import icCeklis from "../assets/icons/ic-ceklis.png";
 
 export default {
   icBni,
@@ -86,4 +87,5 @@ export default {
   icPaste,
   icPaste2,
   icX,
+  icCeklis,
 };
