305da4537b1c23371136758e90697d946d4cd977
Jonathan Richard Sugandhi <jonathanrichard466@gmail.com>
2024-04-30 09:47:16 +0700

    feat: add env variable

--- services/AccountService.js
+++ services/AccountService.js
@@ -2,7 +2,13 @@ import axios from 'axios';
 
 const getAccountData = async () => {
   try {
-    const response = await axios.get('http://192.168.178.104:8080/api/accounts/me');
+
+
+    const response = await axios.get(`${process.env.API_URL}/accounts/me`, {
+      headers: {
+        Authorization: `Bearer ${process.env.API_TOKEN}`
+      }
+    });
     return response.data;
   } catch (error) {
     console.error('Error fetching account data:', error);
