bb19634ebcd8a6e5ea2c65c67ff89648e18ebb03
Muhamad Dani Setiawan <muhamaddanisetiawan@gmail.com>
2024-04-30 15:43:05 +0700

    feat: add scroll refresh indicator in homescreen

--- screens/(tabs)/home.js
+++ screens/(tabs)/home.js
@@ -2,219 +2,18 @@ import {
   View,
   ScrollView,
   Image,
-  StatusBar,
   Text,
   StyleSheet,
   TouchableOpacity,
+  RefreshControl,
 } from "react-native";
 import icons from "../../constants/icons";
 import Octicons from "@expo/vector-icons/Octicons";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { getAccountData } from "../../services/AccountService";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import images from "../../constants/images";
-
-const RekeningComponent = () => {
-  const [accountData, setAccountData] = useState(null);
-  const [showBalance, setShowBalance] = useState(false);
-  useEffect(() => {
-    const getAccount = async () => {
-      setAccountData(await getAccountData());
-    };
-
-    getAccount();
-
-  }, []);
-  return (
-    <View style={{ height: 260 }}>
-      <View
-        style={{
-          backgroundColor: "#F37548",
-          height: "82%",
-          borderBottomLeftRadius: 50,
-          borderBottomRightRadius: 50,
-        }}
-      >
-        <View style={{ marginHorizontal: 16, marginTop: 14 }}>
-          <Text
-            style={{
-              color: "white",
-              fontFamily: "PlusJakartaSansMedium",
-              fontSize: 16,
-            }}
-          >
-            Halo,
-          </Text>
-          <Text
-            style={{
-              color: "white",
-              fontFamily: "PlusJakartaSansBold",
-              fontSize: 16,
-            }}
-          >
-            {accountData && accountData.data.customer_name}
-          </Text>
-        </View>
-      </View>
-      <View style={{ backgroundColor: "blue" }}>
-        <View
-          style={{
-            marginHorizontal: 16,
-            position: "absolute",
-            top: 0,
-            left: 0,
-            right: 0,
-            marginTop: -130,
-            paddingHorizontal: 16,
-            paddingVertical: 14,
-            backgroundColor: "white",
-            borderRadius: 16,
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 0,
-              height: 4,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 18,
-            elevation: 5,
-          }}
-        >
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-            }}
-          >
-            <Text
-              style={{
-                fontFamily: "PlusJakartaSansBold",
-                color: "#5D6B82",
-              }}
-            >
-              BNI Taplus Muda
-            </Text>
-            <View
-              style={{
-                height: 26,
-                backgroundColor: "#F5F6F7",
-                flexDirection: "row",
-                paddingHorizontal: 8,
-                paddingVertical: 4,
-                borderRadius: 50,
-              }}
-            >
-              <Image
-                source={icons.icPoinHome}
-                style={{
-                  height: 15,
-                  width: 32,
-                }}
-                resizeMode="contain"
-              />
-              <Text
-                style={{
-                  color: "#F15922",
-                  fontSize: 12,
-                  fontFamily: "PlusJakartaSansBold",
-                }}
-              >
-                3.165
-              </Text>
-              <Image
-                source={icons.icArrowRightPoin}
-                style={{
-                  height: 10,
-                  width: 10,
-                  alignSelf: "center",
-                }}
-                resizeMode="contain"
-              />
-            </View>
-          </View>
-          <View style={{ flexDirection: "row", gap: 5, alignItems: "center" }}>
-            <Text
-              style={{
-                color: "#5D6B82",
-                fontFamily: "PlusJakartaSansRegular",
-              }}
-            >
-              {accountData && accountData.data.account_number}
-            </Text>
-            <Octicons name="copy" size={16} color="#98A1B0" />
-          </View>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 8,
-              marginTop: 14,
-            }}
-          >
-            {showBalance ? (
-              <Text
-                style={{
-                  fontFamily: "PlusJakartaSansBold",
-                  fontSize: 16,
-                  color: "#5D6B82",
-                }}
-              >
-                Rp.{accountData && accountData.data.balance}
-              </Text>
-            ) : (
-              <TouchableOpacity onPress={() => setShowBalance(true)}>
-                <Text
-                  style={{
-                    fontFamily: "PlusJakartaSansBold",
-                    fontSize: 16,
-                    color: "#5D6B82",
-                  }}
-                >
-                  Rp.******
-                </Text>
-              </TouchableOpacity>
-            )}
-            <TouchableOpacity
-              onPress={() => setShowBalance(!showBalance)}
-              style={{ marginTop: 4 }}
-            >
-              <Ionicons
-                name={showBalance ? "eye-outline" : "eye-off-outline"}
-                size={18}
-                color="#5D6B82"
-              />
-            </TouchableOpacity>
-          </View>
-
-          <View
-            style={{
-              borderBottomColor: "#E5E5E5",
-              borderBottomWidth: 1,
-              marginTop: 14,
-            }}
-          />
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              marginTop: 14,
-            }}
-          >
-            <Text
-              style={{
-                fontFamily: "PlusJakartaSansRegular",
-                color: "#5D6B82",
-              }}
-            >
-              Semua Rekeningmu
-            </Text>
-            <Ionicons name="chevron-down" size={18} color="#5D6B82" />
-          </View>
-        </View>
-      </View>
-    </View>
-  );
-};
 
 const data = [
   {
@@ -344,11 +143,227 @@ const PromotionComponent = () => {
 };
 
 export default function Home() {
+  const [accountData, setAccountData] = useState(null);
+  const [showBalance, setShowBalance] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    const getAccount = async () => {
+      setAccountData(await getAccountData());
+    };
+
+    getAccount();
+  }, []);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+      // const getAccount = async () => {
+      //   setAccountData(await getAccountData());
+      // };
+
+      // getAccount();
+    }, 500);
+  }, []);
+
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
         <View>
-          <RekeningComponent />
+          <View style={{ height: 260 }}>
+            <View
+              style={{
+                backgroundColor: "#F37548",
+                height: "82%",
+                borderBottomLeftRadius: 50,
+                borderBottomRightRadius: 50,
+              }}
+            >
+              <View style={{ marginHorizontal: 16, marginTop: 14 }}>
+                <Text
+                  style={{
+                    color: "white",
+                    fontFamily: "PlusJakartaSansMedium",
+                    fontSize: 16,
+                  }}
+                >
+                  Halo,
+                </Text>
+                <Text
+                  style={{
+                    color: "white",
+                    fontFamily: "PlusJakartaSansBold",
+                    fontSize: 16,
+                  }}
+                >
+                  {accountData && accountData.data.customer_name}
+                </Text>
+              </View>
+            </View>
+            <View style={{ backgroundColor: "blue" }}>
+              <View
+                style={{
+                  marginHorizontal: 16,
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  marginTop: -130,
+                  paddingHorizontal: 16,
+                  paddingVertical: 14,
+                  backgroundColor: "white",
+                  borderRadius: 16,
+                  shadowColor: "#000",
+                  shadowOffset: {
+                    width: 0,
+                    height: 4,
+                  },
+                  shadowOpacity: 0.25,
+                  shadowRadius: 18,
+                  elevation: 5,
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontFamily: "PlusJakartaSansBold",
+                      color: "#5D6B82",
+                    }}
+                  >
+                    BNI Taplus Muda
+                  </Text>
+                  <View
+                    style={{
+                      height: 26,
+                      backgroundColor: "#F5F6F7",
+                      flexDirection: "row",
+                      paddingHorizontal: 8,
+                      paddingVertical: 4,
+                      borderRadius: 50,
+                    }}
+                  >
+                    <Image
+                      source={icons.icPoinHome}
+                      style={{
+                        height: 15,
+                        width: 32,
+                      }}
+                      resizeMode="contain"
+                    />
+                    <Text
+                      style={{
+                        color: "#F15922",
+                        fontSize: 12,
+                        fontFamily: "PlusJakartaSansBold",
+                      }}
+                    >
+                      3.165
+                    </Text>
+                    <Image
+                      source={icons.icArrowRightPoin}
+                      style={{
+                        height: 10,
+                        width: 10,
+                        alignSelf: "center",
+                      }}
+                      resizeMode="contain"
+                    />
+                  </View>
+                </View>
+                <View
+                  style={{ flexDirection: "row", gap: 5, alignItems: "center" }}
+                >
+                  <Text
+                    style={{
+                      color: "#5D6B82",
+                      fontFamily: "PlusJakartaSansRegular",
+                    }}
+                  >
+                    {accountData && accountData.data.account_number}
+                  </Text>
+                  <Octicons name="copy" size={16} color="#98A1B0" />
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    gap: 8,
+                    marginTop: 14,
+                  }}
+                >
+                  {showBalance ? (
+                    <Text
+                      style={{
+                        fontFamily: "PlusJakartaSansBold",
+                        fontSize: 16,
+                        color: "#5D6B82",
+                      }}
+                    >
+                      Rp.{accountData && accountData.data.balance}
+                    </Text>
+                  ) : (
+                    <TouchableOpacity onPress={() => setShowBalance(true)}>
+                      <Text
+                        style={{
+                          fontFamily: "PlusJakartaSansBold",
+                          fontSize: 16,
+                          color: "#5D6B82",
+                        }}
+                      >
+                        Rp.******
+                      </Text>
+                    </TouchableOpacity>
+                  )}
+                  <TouchableOpacity
+                    onPress={() => setShowBalance(!showBalance)}
+                    style={{ marginTop: 4 }}
+                  >
+                    <Ionicons
+                      name={showBalance ? "eye-outline" : "eye-off-outline"}
+                      size={18}
+                      color="#5D6B82"
+                    />
+                  </TouchableOpacity>
+                </View>
+
+                <View
+                  style={{
+                    borderBottomColor: "#E5E5E5",
+                    borderBottomWidth: 1,
+                    marginTop: 14,
+                  }}
+                />
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    marginTop: 14,
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontFamily: "PlusJakartaSansRegular",
+                      color: "#5D6B82",
+                    }}
+                  >
+                    Semua Rekeningmu
+                  </Text>
+                  <Ionicons name="chevron-down" size={18} color="#5D6B82" />
+                </View>
+              </View>
+            </View>
+          </View>
           <MenuComponent />
           <PromotionComponent />
         </View>
