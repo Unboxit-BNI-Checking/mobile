f68ee1866a4cfb7c4e698fbc5d58bf258b1aab27
Muhamad Dani Setiawan <muhamaddanisetiawan@gmail.com>
2024-05-06 09:04:44 +0700

    feat: add Rp Text

--- component/text/RupiahFormatComponent.js
+++ component/text/RupiahFormatComponent.js
@@ -5,7 +5,7 @@ const RupiahFormatComponent = ({ value }) => {
     .toString()
     .replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
-  return <Text>{formattedAmount}</Text>;
+  return <Text>Rp{formattedAmount}</Text>;
 };
 
 export default RupiahFormatComponent;
